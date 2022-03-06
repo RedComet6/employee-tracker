4a703499df1de24ae4fa8d30be2fa18a80f3ea6b
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-06 17:54:57 -0600

    working connection to db

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
 require("dotenv").config();
-
-const PORT = process.env.PORT || 3001;
+console.log(process.env);
 
 const db = mysql.createConnection(
     {
         host: "localhost",
-        port: PORT,
+        port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
     },
-    console.log(`Connected to the books_db database.`)
+    console.log(`Connected to the workplace_db database.`)
 );
