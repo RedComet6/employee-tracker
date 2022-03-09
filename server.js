7fb004d039e6f2492e4aa9c02ddf8281814dbdcb
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-08 19:57:31 -0600

    testing view all departments

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) throw err;
     console.log(`Connected to the workplace_db database.`);
-    init();
+    // init();
+    viewAllDepartments();
 });
 
 async function init() {
@@ -22,10 +23,17 @@ async function init() {
         name: "choice",
         message: "What would you like to do?",
         type: "list",
-        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
+        choices: [{ name: "View all departments", value: "viewDepartments" }, "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Quit"],
     };
 
     await inquirer.prompt(choice).then((answer) => {
         console.log(answer);
+        // switch case with separate functions
+    });
+}
+
+function viewAllDepartments() {
+    db.query("SELECT * FROM departments", (err, res) => {
+        console.table(res);
     });
 }
