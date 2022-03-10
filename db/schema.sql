d4fd0485f00fe6b8560389471816173d4d00090e
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-09 20:49:15 -0600

    updated employees table to have manager id reference employee id, updated employees seed accordingly

--- db/schema.sql
+++ db/schema.sql
@@ -20,6 +20,6 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
+    manager_id INT REFERENCES employees(id),
     FOREIGN KEY (role_id) REFERENCES roles(id)
 );
